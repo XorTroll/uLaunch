cb94963701000edb735e9547b698efa6aebee042
XorTroll <xabofdez@outlook.es>
2019-10-14 19:29:44 +0200

    Properly handle hbmenu NRO specially

--- Common/Source/hb/hb_Target.cpp
+++ Common/Source/hb/hb_Target.cpp
@@ -10,6 +10,8 @@ namespace hb
     void Target(TargetInput input, bool once)
     {
         int ctr = once ? 1 : -1;
+        // If we're loading hbmenu, it makes no sense to load the NRO once (opening a homebrew would terminate it, what literally goes against hbmenu's main functionality)
+        if(strcmp("sdmc:/hbmenu.nro", input.nro_path) == 0) ctr = -1;
         hb_hbl_Target(input.nro_path, input.argv, ctr);
     }
 }
