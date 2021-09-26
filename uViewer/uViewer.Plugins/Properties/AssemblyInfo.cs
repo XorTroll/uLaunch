3ef0007efa7817b59c9f833a1e783d0053ee6da3
XorTroll <xabofdez@gmail.com>
2021-09-26 17:10:55 +0200

    Late, but bump version to 0.3.1

--- uViewer/uViewer.Plugins/Properties/AssemblyInfo.cs
+++ uViewer/uViewer.Plugins/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
 // utilizando el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.0.0")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyVersion("0.3.1.0")]
+[assembly: AssemblyFileVersion("0.3.1.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
