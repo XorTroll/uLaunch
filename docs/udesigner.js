5fedf0fdeb2cf7eb3fdb56e32610c0855b3808af
XorTroll <xortrolldev@proton.me>
2025-03-16 21:58:29 +0100

    Also bump uScreen version

--- docs/udesigner.js
+++ docs/udesigner.js
@@ -31,7 +31,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpyho90kyc.js
+// include: /tmp/tmpf2o1wptr.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -212,21 +212,21 @@ Module['FS_createPath']("/", "assets", true, true);
 
   })();
 
-// end include: /tmp/tmpyho90kyc.js
-// include: /tmp/tmpwrai_7_w.js
+// end include: /tmp/tmpf2o1wptr.js
+// include: /tmp/tmpsm3lx101.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmpwrai_7_w.js
-// include: /tmp/tmp09o9lafe.js
+  // end include: /tmp/tmpsm3lx101.js
+// include: /tmp/tmpt7a6up5g.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmp09o9lafe.js
+  // end include: /tmp/tmpt7a6up5g.js
 
 
 // Sometimes an existing Module object exists with properties
