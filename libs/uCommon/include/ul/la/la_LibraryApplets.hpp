19e0f0aeb86b0497b42e29a23ba2692434ff230b
XorTroll <xortrolldev@proton.me>
2025-05-11 22:50:31 +0200

    Add new header

---
+++ libs/uCommon/include/ul/la/la_LibraryApplets.hpp
@@ -0,0 +1,25 @@
+
+#pragma once
+#include <ul/ul_Include.hpp>
+
+namespace ul::la {
+
+    constexpr u64 LibraryAppletAuth = 0x0100000000001001;
+    constexpr u64 LibraryAppletCabinet = 0x0100000000001002;
+    constexpr u64 LibraryAppletController = 0x0100000000001003;
+    constexpr u64 LibraryAppletDataErase = 0x0100000000001004;
+    constexpr u64 LibraryAppletError = 0x0100000000001005;
+    constexpr u64 LibraryAppletNetConnect = 0x0100000000001006;
+    constexpr u64 LibraryAppletPlayerSelect = 0x0100000000001007;
+    constexpr u64 LibraryAppletSwkbd = 0x0100000000001008;
+    constexpr u64 LibraryAppletMiiEdit = 0x0100000000001009;
+    constexpr u64 LibraryAppletWeb = 0x010000000000100A;
+    constexpr u64 LibraryAppletShop = 0x010000000000100B;
+    constexpr u64 LibraryAppletPhotoViewer = 0x010000000000100D;
+    constexpr u64 LibraryAppletSet = 0x010000000000100E;
+    constexpr u64 LibraryAppletOfflineWeb = 0x010000000000100F;
+    constexpr u64 LibraryAppletLoginShare = 0x0100000000001010;
+    constexpr u64 LibraryAppletWifiWebAuth = 0x0100000000001011;
+    constexpr u64 LibraryAppletMyPage = 0x0100000000001013;
+
+}
